becfe2977e6cb00bd0210084ba78a533816f5839
Arturo Villa <villa@Arturos-MacBook-Air.local>
2020-05-13 16:54:53 -0500

    Funcionalidades agregadas: ataque

--- app/src/main/java/com/example/proyectzero/GameActivity.kt
+++ app/src/main/java/com/example/proyectzero/GameActivity.kt
@@ -12,16 +12,22 @@ class GameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
-
+        iniciar()
     }
-
     var live = 200
     var damage = 0
     var heal = 0
     var score = 0
+    var monsterDamage = 0
     var monsterLive = (100 until 280).random()
 
-    public fun getAttackDamage(number: Number): Int {
+    fun iniciar(){
+        textMonsterLive.text = monsterLive.toString()
+        textGame.text = "Oh no un mounstro ha aparecido"
+        textLive.text = live.toString()
+    }
+
+     fun getAttackDamage(number: Number): Int {
         var number = (0 until 30).random()
         var item = (3..6).random()
         if(number >= 8){
@@ -30,13 +36,23 @@ class GameActivity : AppCompatActivity() {
         return number
     }
 
+    fun getMonsterAttack(number: Number): Int
+    {
+        var number = (0 until 30).random()
+        return number
+
+    }
     fun attackPress(view: View) {
+        textGame.text = "Atacaste al enemigo"
+        
         damage = getAttackDamage(damage)
+        textDamageToMonster.text = damage.toString()
         monsterLive -= damage
         score = damage * 10
     }
 
     fun healPress(view: View) {
+        textGame.text="Te haz curado"
         heal = (0..10).random()
         if (live >= 30) {
             heal *= 2
@@ -47,7 +63,10 @@ class GameActivity : AppCompatActivity() {
         live += heal
     }
     fun retreatPress(view: View) {
-        
+        textGame.text = "RETIRADA!!!"
+        monsterDamage = getMonsterAttack(monsterDamage)
+        textMonsterDamage.text = monsterDamage.toString()
+
     }
 
 
